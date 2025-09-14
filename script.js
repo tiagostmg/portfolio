23b06ef6644b3362668f5bd2456ef8adedaffe63
Tiago <tiago.taumaturgo@gmail.com>
2025-09-14 17:22:39 -0300

    .

--- script.js
+++ script.js
@@ -113,7 +113,8 @@ window.addEventListener("scroll", scrollUp);
 //dark light mode------------------
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
-const iconTheme = "fa-sun";
+const iconMoon = "fa-moon";
+const iconSun = "fa-sun";
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem("selected-theme");
@@ -123,25 +124,26 @@ const selectedIcon = localStorage.getItem("selected-icon");
 const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "fa-moon" : "fa-sun";
+  themeButton.classList.contains(iconMoon) ? iconMoon : iconSun;
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
-    darkTheme
-  );
-  themeButton.classList[selectedIcon === "fa-moon" ? "add" : "remove"](
-    iconTheme
-  );
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList.remove(iconMoon, iconSun);
+  themeButton.classList.add(selectedIcon === iconMoon ? iconMoon : iconSun);
 }
+
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
-  themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
+  if (themeButton.classList.contains(iconMoon)) {
+    themeButton.classList.remove(iconMoon);
+    themeButton.classList.add(iconSun);
+  } else {
+    themeButton.classList.remove(iconSun);
+    themeButton.classList.add(iconMoon);
+  }
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
